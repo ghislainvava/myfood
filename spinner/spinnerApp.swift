68e4f38452eb24bbcf882062b6e8694b7e4b049a
ghislainvava <ghislainvachet@yahoo.fr>
2021-09-16 09:56:46 +0200

    Initial Commit

---
+++ spinner/spinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  spinnerApp.swift
+//  spinner
+//
+//  Created by Vachet Ghislain on 16/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct spinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
